1144c68ac8de021919a16646c49ae9fcf8befc74
ftk1nsy <gogsadmin@fabric8.local>
2016-10-14 10:15:13 -0400

    added ActiveMQ initialization from MainApp

--- idugdemo/src/main/java/com/ups/cra/icc/idugroute/idugdemo/MainApp.java
+++ idugdemo/src/main/java/com/ups/cra/icc/idugroute/idugdemo/MainApp.java
@@ -1,16 +1,31 @@
 package com.ups.cra.icc.idugroute.idugdemo;
 
-import org.apache.camel.main.Main;
+import org.apache.activemq.camel.component.ActiveMQComponent;
+import org.apache.camel.CamelContext;
+import org.apache.camel.impl.DefaultCamelContext;
 
 public class MainApp {
 
 	public static void main(String[] args) throws Exception {
-		
-		Main main = new Main();
-		main.enableHangupSupport();
-		main.addRouteBuilder(new ShipmentRoute());
-		main.run(args);
 
+		
+		CamelContext context = new DefaultCamelContext();
+		ActiveMQComponent activeMQComponent = new ActiveMQComponent();
+	    activeMQComponent.setBrokerURL("tcp://localhost:61616");
+	    activeMQComponent.setUserName("admin");
+	    activeMQComponent.setPassword("admin");
+	   
+		context.addComponent("activemq", activeMQComponent);
+		context.addRoutes(new ShipmentRoute());
+		context.start();
+		
+		
+		/*
+		 Main main = new Main(); 
+		 main.enableHangupSupport();
+		 main.addRouteBuilder(new ShipmentRoute());
+		 main.run(args);
+		*/ 
 	}
 
 }
