f49c8014a7d15adc8413ae9fa3bfacfa08185739
ftk1nsy <gogsadmin@fabric8.local>
2016-10-17 16:47:36 -0400

    added properties file

--- idugdemo/src/main/java/com/ups/cra/icc/idugroute/idugdemo/MainApp.java
+++ idugdemo/src/main/java/com/ups/cra/icc/idugroute/idugdemo/MainApp.java
@@ -1,5 +1,8 @@
 package com.ups.cra.icc.idugroute.idugdemo;
 
+import java.io.FileInputStream;
+import java.util.Properties;
+
 import org.apache.activemq.camel.component.ActiveMQComponent;
 import org.apache.camel.CamelContext;
 import org.apache.camel.impl.DefaultCamelContext;
@@ -8,12 +11,16 @@ public class MainApp {
 
 	public static void main(String[] args) throws Exception {
 
+		Properties defaultProps = new Properties();
+		FileInputStream in = new FileInputStream("./src/main/resources/idug.properties");
+		defaultProps.load(in);
+		in.close();
 		
 		CamelContext context = new DefaultCamelContext();
 		ActiveMQComponent activeMQComponent = new ActiveMQComponent();
 		//activeMQComponent.setBrokerURL("http://www.tcp.amq.idug2016.link:80");
-		//activeMQComponent.setBrokerURL("tcp://www.tcp.amq.idug2016.link:80");
-		activeMQComponent.setBrokerURL("tcp://172.30.84.159:61616");
+		activeMQComponent.setBrokerURL(defaultProps.getProperty("brokerURL"));
+		//activeMQComponent.setBrokerURL("tcp://172.30.84.159:61616");
 		activeMQComponent.setUserName("admin");
 	    activeMQComponent.setPassword("admin");
 	   
